dfb13b890507156494d50d3f07eec99b2ba9d8b9
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-02-06 15:27:41 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class beg4
+
+{
+
+public static void main(String args[])
+
+{
+
+char ch;
+
+Scanner scan = new Scanner(System.in);
+
+System.out.print("Input:\n");
+
+ch=scan.next().charAt(0);
+
+System.out.print("\n"+ch+"\n");
+
+System.out.println("Output:\n");
+
+if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+
+{
+
+System.out.print("Alphabet");
+
+}
+
+else
+
+{
+
+System.out.print("No");
+
+}
+
+}
+
+}
