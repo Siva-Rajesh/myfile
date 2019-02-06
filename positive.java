ea0b8fcf51e5f5059188bb11bb2b055c40d7b3ae
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-02-06 14:10:36 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,22 @@
+class PosNegZero
+{
+    public static void main(String []s)
+    {
+        int num;
+       
+        Scanner sc=new Scanner(System.in);
+         
+        System.out.print("");
+        num=sc.nextInt();
+         
+       
+        if(num>0)
+            System.out.println(POSITIVE ");
+        else if(num<0)
+            System.out.println(NEGATIVE ");
+        else
+            System.out.println(ZERO.");
+         
+    }
+}
+
