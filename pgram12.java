d59c2e121e3fb1608b0f85c8e2e55e233e32584f
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-02-18 15:35:04 +0530

    Create pgram12.java

---
+++ pgram12.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+  class pali
+{
+    public static void main(String args[])
+    {
+        int number, m, a = 0,x;
+        Scanner input = new Scanner(System.in);
+         
+        number = input.nextInt();
+        m = number;
+     
+        while(number > 0)
+        {
+            x = number % 10;
+            a = a * 10 + x;
+            number = number / 10;
+        }
+        if(a == m)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        
+        
+    }
+}
