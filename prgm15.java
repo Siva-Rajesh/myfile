ddbddce029b04e1e0f078b3a76958f32dbc08490
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-03-05 11:37:05 +0530

    Create prgm15.java

---
+++ prgm15.java
@@ -0,0 +1,18 @@
+    
+import java.util.Scanner;
+	class eve {
+	    public static void main(String[] args) 
+	    {
+	        Scanner input = new Scanner(System.in);
+	        int s =input.nextInt();
+	        int e =input.nextInt();
+	         for(int i=start+1;i<end;i++)
+	         {
+	             if(i%2==0)
+	             {
+	                 System.out.print(i+"\t");
+	             }
+	         } 
+	        
+	    }
+	}
