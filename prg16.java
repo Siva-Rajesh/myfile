8a2f271b4417c4eaab6442d8030e088ba80647b3
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-03-05 11:39:50 +0530

    Create prg16.java

---
+++ prg16.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;  
+	 class Primeno {  
+	   public static void main(String[] args) {  
+	       Scanner s = new Scanner(System.in);  
+	        
+	       int s = s.nextInt();  
+	        
+	       int e = s.nextInt();   
+	       for (int i = s; i <= e; i++) {  
+	           if (isPrime(i)) {  
+	               System.out.println(i+" ");  
+	           }  
+	       }  
+	   }  
+	   public static boolean isPrime(int n) {  
+	       if (n <= 1) {  
+	           return false;  
+	       }  
+	      for(int i = 2; i <= n/2; ++i)
+	        {
+	            if(n % i == 0)
+	            {
+	                   return false;  
+	            }
+	        }
+	       return true;  
+	   }  
+	}  
