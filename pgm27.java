40a938dc79cd146e9d467bd31b71c51badc6cf1d
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-03-07 15:20:37 +0530

    Create pgm27.java

---
+++ pgm27.java
@@ -0,0 +1,27 @@
+ import java.util.Scanner;
+ class Number{
+
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);  
+        String number = s.next(); 
+        boolean numeric = true;
+
+        try 
+        {
+            Double num = Double.parseDouble(number);
+        } 
+        catch (NumberFormatException e) 
+        {
+            numeric = false;
+        }
+
+        if(numeric)
+          {
+          System.out.println("yes");
+          }
+        else
+        {
+            System.out.println("No");
+        }
+    
