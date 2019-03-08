76215334fba3eec19844b146f6609ed7802a52ab
Siva-Rajesh <47380839+Siva-Rajesh@users.noreply.github.com>
2019-03-08 11:35:22 +0530

    Create pgm41.java

---
+++ pgm41.java
@@ -0,0 +1,17 @@
+ import java.util.*;
+class string
+{  
+public static void main(String args[])  
+{   
+Scanner input=new Scanner(System.in);
+String str=input.next();
+int a=input.nextInt();         
+ for(int i=0;i<a;i++) 
+ {    
+    
+  System.out.println(str);    
+      
+ }    
+  
+}
+} 
